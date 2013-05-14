b73d590877af52fea8318da6c535edf8c0e41dbb
John Maddux <jemaddux@gmail.com>
2013-05-14 17:45:36 -0600

    FIRST

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DungeonMaster::Application.config.secret_token = 'bea8ff81b7f469a039c6c1f28f940c27bdf5d8d946193afdbd14bfef4abd79c1dd271b7f3f19f6a70b0edd5f4eb4e96dddd64f44a4ef39d270e4a617fd531209'
