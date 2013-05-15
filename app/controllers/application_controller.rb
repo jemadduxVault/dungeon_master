f5e32d86438e20d1d0b437077f358e7cb197eed6
John Maddux <jemaddux@gmail.com>
2013-05-14 18:35:50 -0600

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,10 @@ class ApplicationController < ActionController::Base
   def join
 
   end
+
+private
+  def self.nacl(length)
+    x ||= [('a'..'z'),('A'..'Z'),('0'..'9')].map{|i| i.to_a}.flatten
+    (0..(length-1)).map{ x[rand(x.length)] }.join
+  end
 end
